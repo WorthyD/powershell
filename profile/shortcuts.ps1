b1f0fb215eb63600c360f5b0240c9f68664e60ca
Daniel Worthy <danieljworthy@gmail.com>
2026-04-10 11:03:10 -0500

    automated backup date +'04/10/2026 11:03:10'"

--- profile/shortcuts.ps1
+++ profile/shortcuts.ps1
@@ -13,15 +13,7 @@ function notes() {
     cd $env:obsidianPath
 }
 
-function giteager() {
-    git add .
-    git commit -a -m "Stuff"
-    git push
-}
-
-
 function nuke_node { rimraf .\node_modules } 
-function nuke_nx { rimraf .\nx } 
 
 function foo() {
     & "$env:foobarPath\foobar2000.exe"
@@ -43,6 +35,17 @@ function winp() {
     & "$env:psPath\PowerShell\CLEveR.exe" stop
 }
 
+function sh() {
+    # Start Windows SSH Agent service and add keys
+    $sshAgentService = Get-Service ssh-agent -ErrorAction SilentlyContinue
+    if ($sshAgentService.Status -ne 'Running') {
+        Start-Service ssh-agent
+    }
+    # Add SSH keys to agent (suppresses output if already added)
+    ssh-add 2>$null
+
+}
+
 
 function edithost() {
     Start-Process code -ArgumentList C:\Windows\System32\drivers\etc\hosts -Verb RunAs
@@ -53,27 +56,32 @@ function callnvm() {
     $versionDesired = $args[0]
   
     if (($versionDesired -eq "" -Or $versionDesired -eq $null) -And (Test-Path .nvmrc -PathType Any)) {
-      # if we have an nvmrc and no argument supplied, use the version in the file
-      $versionDesired = $(Get-Content .nvmrc).replace( 'v', '' );
+        # if we have an nvmrc and no argument supplied, use the version in the file
+        $versionDesired = $(Get-Content .nvmrc).replace( 'v', '' );
     }
     Write-Host "Requesting version '$($versionDesired)'"
   
     if ($versionDesired -eq "") {
-      Write-Host "A node version needs specifying as an argument if there is no .nvmrc"
-    } else {
-      $response = nvm use $versionDesired;
-      if ($response -match 'is not installed') {
-        if ($response -match '64-bit') {
-          $response = nvm install $versionDesired x64
-        } else {
-          $response = nvm install $versionDesired x86
-        }
-        Write-Host $response
-  
-        $response = nvm use $versionDesired;
-      }
-  
-      Write-Host $response
+        Write-Host "A node version needs specifying as an argument if there is no .nvmrc"
     }
-  }
-  Set-Alias nvmu -value "callnvm"
+    else {
+        $response = nvm use $versionDesired;
+        if ($response -match 'is not installed') {
+            if ($response -match '64-bit') {
+                $response = nvm install $versionDesired x64
+            }
+            else {
+                $response = nvm install $versionDesired x86
+            }
+            Write-Host $response
+  
+            $response = nvm use $versionDesired;
+        }
+  
+        Write-Host $response
+    }
+}
+Set-Alias nvmu -value "callnvm"
+Set-Alias pm -value "pnpm"
+#Set-Alias px -value "pnpm exec"
+function px { pnpm exec @args }
